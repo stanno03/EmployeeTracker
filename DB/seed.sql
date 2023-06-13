ea33774b83b83d0d1ac7c7f9329c73de9cc6f38a
stanno03 <danno_41@hotmail.com>
2023-06-13 17:00:17 +1000

    added helper function to handler CRUD

--- DB/seed.sql
+++ DB/seed.sql
@@ -1,11 +1,20 @@
 -- seed the tables
-INSERT INTO departments (id, name)
+INSERT INTO departments (name)
 VALUES
+('Sales'),
+('Marketing'),
+('Management');
 
 
-INSERT INTO roles (id, title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
+('Salesperson', 70000, 001),
+('Social Media Marketer', 75000, 002),
+('Service Desk Teamlead', 75000, 003);
 
 
-INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
+('Harry', 'Potter', 003, 001),
+('Hermoine', 'Granger', 002, NULL),
+('Ron', 'Weasley', 001, NULL);
